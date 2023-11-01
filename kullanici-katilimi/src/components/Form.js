1bc601b435df084404be39d99ecf8801195df8e4
Murat Simsek <muratsimseekk@gmail.com>
2023-11-01 16:59:47 +0300

    form created

--- kullanici-katilimi/src/components/Form.js
+++ kullanici-katilimi/src/components/Form.js
@@ -3,10 +3,39 @@ import React, { useState } from "react";
 const Formlar = () => {
   const [formData, setFormData] = useState({
     fullName: "",
-    mail: "",
     password: "",
+    mail: "",
     terms: "",
   });
+
+  return (
+    <div>
+      <h2>Membership Form:</h2>
+      <form>
+        <label>
+          Full Name :
+          <input type="text" name="fullName" />
+        </label>
+        <br />
+        <label>
+          Password :
+          <input type="password" name="password" />
+        </label>
+        <br />
+        <label>
+          E-mail :
+          <input type="email" name="mail" />
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" name="subscribe" />I have read and agree to the
+          terms of services.
+        </label>
+        <br />
+        <button>Submit ! </button>
+      </form>
+    </div>
+  );
 };
 
 export default Formlar;
